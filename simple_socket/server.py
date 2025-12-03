b28cacf42c7624b5d15da4c193318ab4d6e096b6
Rashmika-j5ck <rashmikamadhubhashitha@gmail.com>
2025-12-04 01:31:20 +0530

    Add files via upload

---
+++ simple_socket/server.py
@@ -0,0 +1,23 @@
+import socket
+
+
+s = socket.socket()
+print("socket created")
+
+s.bind(("localhost",9999))
+
+
+s.listen(3)
+print("waiting for connections")
+
+while True:
+    c,addr = s.accept()
+    name = c.recv(1024).decode()
+
+    print("Connected with ", addr,name)
+
+
+    c.send(bytes("Welcome to Telusko","utf-8"))
+
+
+c.close()
